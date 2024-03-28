06c5d3536bbfc837bab8747cabe03b48aa2b81e6
Nargis <nargissamatova16@gmail.com>
2024-03-28 12:24:44 +0200

    conncet

--- PHP-MAMP/phpDir/src/CRUDApp2/login.php
+++ PHP-MAMP/phpDir/src/CRUDApp2/login.php
@@ -1,7 +1,49 @@
 <?php
 if(isset($_POST['submit'])){
-    echo 'yes, we got data';
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+   // echo $user . " " . $pass;
+
 }
+
+// validate the form
+
+if($user && $pass){
+    echo $user . " " . $pass;
+}else{
+    echo 'username and password cannot be blank';
+}
+
+$host = 'db';
+
+//database user name
+$dbname = 'loginapp';
+$dbuser = 'root';
+$dbpass = 'lionPass';
+
+// check the mysql connection status
+$conn = new mysqli($host, $dbuser, $dbpass, $dbname);
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    echo "Connected to MySQL server successfully";
+}
+
+// create the records inside db
+
+$query = "INSERT INTO users(username, password)";
+$query .= "VALUES('$user', '$pass')";
+
+$result = mysqli_query($conn, $query);
+if(!$result){
+    die('Query insertation failed');
+}
+
+
+
+
+
+
 ?>
 
 <form action="login.php" method="post">
