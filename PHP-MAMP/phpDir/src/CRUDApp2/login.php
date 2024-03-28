283585623fda224e7ed60f7dd0867da5a9484b4c
Nargis <nargissamatova16@gmail.com>
2024-03-28 11:42:36 +0200

    updates

---
+++ PHP-MAMP/phpDir/src/CRUDApp2/login.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST['submit'])){
+    echo 'yes, we got data';
+}
+?>
+
+<form action="login.php" method="post">
+    <label for="username">Username</label>
+    <input type="text" name="username">
+    <label for="password">Password</label>
+    <input type="password" name="password">
+    <input type="submit" name="submit" value="submit">
+</form>
