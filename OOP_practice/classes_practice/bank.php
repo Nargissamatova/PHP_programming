8d7ab8fbecec2badc65aac37c758cd3ddd820a2b
Nargis <nargissamatova16@gmail.com>
2024-03-30 22:56:26 +0200

    changes to bank transactions

--- OOP_practice/classes_practice/bank.php
+++ OOP_practice/classes_practice/bank.php
@@ -9,6 +9,8 @@ Create a Class for a Simple Bank Account:
 5. Create an instance of the BankAccount class and test its methods with various deposit and withdrawal scenarios.
 
 */
+// Get the current date and time
+$currentDateTime = date("Y-m-d H:i:s");
 
 class BankAccount
 {
@@ -23,13 +25,56 @@ class BankAccount
         $this->ownerName = $ownerName;
     }
     // Methods
-    public function deposit()
+    public function setDeposit($deposit)
     {
+        return $deposit;
     }
-    public function withdraw()
+    public function setWithdraw($withdrawAmount)
     {
+        if ($this->balance < $withdrawAmount) {
+            echo 'Your account balance should be higher than the withdrawal amount ';
+        } else {
+            return $withdrawAmount;
+        }
     }
     public function checkBalance()
     {
+        // Method to check balance (return the current balance)
+        return $this->balance;
     }
 }
+
+$test = new BankAccount(324665, 50, 'John Doe');
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST["checkBalance"])) {
+    $finalBalance = $test->checkBalance();
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Bank account</h1>
+    <form action="bank.php" method="post">
+        <h3>Balance: <?= $test->balance ?>€</h3>
+        <h3>Owner: <?= $test->ownerName ?></h3>
+        <h3>Deposit: <span style="color:green"> +<?= $test->setDeposit(33) ?>€ </span></h3>
+        <h3>Withdraw: <span style="color: red">-<?= $test->setWithdraw(13) ?>€</span></h3>
+        <input type="submit" value="Check Balance" name="checkBalance">
+        <hr>
+        <?php if (isset($finalBalance)) : ?>
+            <h3>Overall: <?= $finalBalance ?></h3>
+        <?php endif; ?>
+    </form>
+</body>
+
+</html>
