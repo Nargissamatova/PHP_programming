8eb56dc208538e86b130503e7dfcd6d9a75f8c18
Nargis <nargissamatova16@gmail.com>
2024-04-01 12:13:41 +0300

    completed

--- calculator/Calculator.php
+++ calculator/Calculator.php
@@ -58,39 +58,49 @@ $test = new Calculator($num1, $num2);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>PHP calculator</title>
 </head>
 
 <body>
-    <h2>Simple calculator</h2>
-    <form action="Calculator.php" method="get">
-        <input type="number" name="num1">
-        <select name="operation">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">x</option>
-            <option value="/">%</option>
+    <div class="container">
+        <h2>Simple calculator</h2>
+        <form action="Calculator.php" method="get">
+            <input type="number" name="num1">
+            <select name="operation">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">x</option>
+                <option value="/">/</option>
 
-        </select>
-        <input type="number" name="num2">
-        <input type="submit" value="submit" name="submit">
-    </form>
-    <p>
-        <?php
-        // Check if the form was submitted
-        if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['submit'])) {
-            if ($_GET['operation'] == '+') {
-                echo 'The answer is: ' . $test->addition();
-            } elseif ($_GET['operation'] == '-') {
-                echo 'The answer is: ' . $test->subtraction();
-            } elseif ($_GET['operation'] == '*') {
-                echo 'The answer is: ' . $test->multiplication();
-            } elseif ($_GET['operation'] == '/') {
-                echo 'The answer is: ' . $test->division();
+            </select>
+            <input type="number" name="num2">
+            <input type="submit" value="submit" name="submit">
+        </form>
+        <p>
+            <?php
+            // Check if the form was submitted
+            if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['submit'])) {
+                // check if number is entered
+                if (empty($_GET['num1']) || empty($_GET['num2'])) {
+                    echo '<span style="color:crimson">Please enter a number for both fields.</span>';
+                } else {
+                    // check type of operation
+                    if ($_GET['operation'] == '+') {
+                        echo 'The answer is: ' . $test->addition();
+                    } elseif ($_GET['operation'] == '-') {
+                        echo 'The answer is: ' . $test->subtraction();
+                    } elseif ($_GET['operation'] == '*') {
+                        echo 'The answer is: ' . $test->multiplication();
+                    } elseif ($_GET['operation'] == '/') {
+                        echo 'The answer is: ' . $test->division();
+                    }
+                }
             }
-        }
-        ?>
-    </p>
+            ?>
+        </p>
+    </div>
+
 </body>
 
 </html>
