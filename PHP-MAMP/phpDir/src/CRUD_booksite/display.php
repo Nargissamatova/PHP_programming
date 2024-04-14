8e61d3be33bf0e51ed94f57c6dc43fe38132ade1
Nargis <nargissamatova16@gmail.com>
2024-04-14 16:14:09 +0300

    book info displayed

--- PHP-MAMP/phpDir/src/CRUD_booksite/display.php
+++ PHP-MAMP/phpDir/src/CRUD_booksite/display.php
@@ -1,3 +1,17 @@
+<?php
+require_once 'db.php';
+
+// Retrieve all books from the 'books' table
+$result = $conn->query("SELECT * FROM books");
+
+// Initialize an array to hold the book data
+$rows = array();
+// Fetch each row of book data and add it to the $rows array
+while ($row = $result->fetch_assoc()) {
+    $rows[] = $row;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,6 +27,43 @@
         <button class="btn btn-primary my-5">
             <a class="text-light" href="addbook.php">Add book </a>
         </button>
+    </div>
+    <div class="container">
+        <h2>Book Information</h2>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Author</th>
+                    <th>Publishing Year</th>
+                    <th>Genre</th>
+                    <th>Update</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if ($result->num_rows > 0) : ?>
+                    <?php foreach ($result as $row) : ?>
+                        <tr data-id="<?= $row['id'] ?>">
+                            <td><?= $row['id'] ?></td>
+                            <td><?= $row['title'] ?></td>
+                            <td><?= $row['description'] ?></td>
+                            <td><?= $row['author'] ?></td>
+                            <td><?= $row['publishing_year'] ?></td>
+                            <td><?= $row['genre'] ?></td>
+                            <td><button class="btn btn-primary">Update</button></td>
+                            <td><button class="btn btn-danger">Delete</button></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php else : ?>
+                    <tr>
+                        <td>No results</td>
+                    </tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
     </div>
 </body>
 
