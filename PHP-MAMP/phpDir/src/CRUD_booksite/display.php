e8198077c511f1ecf8735d4681c832729bf9f079
Nargis <nargissamatova16@gmail.com>
2024-04-16 00:01:31 +0300

    animations added

--- PHP-MAMP/phpDir/src/CRUD_booksite/display.php
+++ PHP-MAMP/phpDir/src/CRUD_booksite/display.php
@@ -19,17 +19,19 @@ while ($row = $result->fetch_assoc()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="animations.css">
+
     <title>Document</title>
 </head>
 
 <body>
     <div class="container">
-        <button class="btn btn-primary my-5">
+        <button class="btn btn-success my-5">
             <a class="text-light" href="addbook.php">Add book </a>
         </button>
     </div>
     <div class="container">
-        <h2>Book Information</h2>
+        <h2 class="text-light">Book Information</h2>
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -53,7 +55,7 @@ while ($row = $result->fetch_assoc()) {
                             <td><?= $row['author'] ?></td>
                             <td><?= $row['publishing_year'] ?></td>
                             <td><?= $row['genre'] ?></td>
-                            <td><button class="btn btn-secondary"><a href="update.php?updateid=<?= $row['id'] ?>" class="text-light">Update</a></button></td>
+                            <td><button class="btn btn-primary"><a href="update.php?updateid=<?= $row['id'] ?>" class="text-light">Update</a></button></td>
                             <td><button class=" btn btn-danger"><a href="delete.php?deleteid=<?= $row['id'] ?>" class="text-light">Delete</a></button></td>
                         </tr>
                     <?php endforeach; ?>
