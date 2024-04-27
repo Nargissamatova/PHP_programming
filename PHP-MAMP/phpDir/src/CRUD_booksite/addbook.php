8997b07b6d3215ea8026f0835acbd9e566811564
Nargis <nargissamatova16@gmail.com>
2024-04-27 14:05:34 +0300

    error handling and validation for empty fields

--- PHP-MAMP/phpDir/src/CRUD_booksite/addbook.php
+++ PHP-MAMP/phpDir/src/CRUD_booksite/addbook.php
@@ -1,30 +1,49 @@
 <?php
 include 'db.php';
 
+$errors = []; // array to store validation errors
 
 if (isset($_POST['submit'])) {
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-    $author = $_POST['author'];
-    $year = $_POST['year'];
-    $genre = $_POST['genre'];
+    if (empty($_POST['title'])) {
+        $errors['title'] = 'Please enter a title.';
+    }
+    if (empty($_POST['author'])) {
+        $errors['author'] = 'Please enter an author.';
+    }
+    if (empty($_POST['year'])) {
+        $errors['year'] = 'Please enter a year.';
+    } elseif (!is_numeric($_POST['year'])) {
+        $errors['year'] = 'Please enter a valid year.';
+    }
+    if (empty($_POST['description'])) {
+        $errors['description'] = 'Please enter a description.';
+    }
 
-    // Prepare SQL statement
-    $sql = "INSERT INTO books (title, description, author, publishing_year, genre)
-            VALUES ('$title', '$description', '$author', '$year', '$genre')";
+    // If no validation errors, proceed with inserting data into the database
+    if (empty($errors)) {
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $author = $_POST['author'];
+        $year = $_POST['year'];
+        $genre = $_POST['genre'];
 
-    // Execute SQL statement
-    $result = mysqli_query($conn, $sql);
+        // Prepare SQL statement
+        $sql = "INSERT INTO books (title, description, author, publishing_year, genre)
+                VALUES ('$title', '$description', '$author', '$year', '$genre')";
 
-    if ($result) {
-        header('Location: display.php');
-    } else {
-        echo 'Error: ' . $sql . '<br>' . mysqli_error($conn);
+        // Execute SQL statement
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            header('Location: display.php');
+            exit(); // stop further execution
+        } else {
+            echo 'Error: ' . $sql . '<br>' . mysqli_error($conn);
+        }
     }
 }
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -45,14 +64,23 @@ if (isset($_POST['submit'])) {
             <div class="form-group">
                 <label for="exampleFormControlInput1">Title</label>
                 <input type="text" name="title" class="form-control" placeholder="Atomic Habits">
+                <?php if (isset($errors['title'])) : ?>
+                    <div class="text-danger"><?php echo $errors['title']; ?></div>
+                <?php endif; ?>
             </div>
             <div class="form-group">
                 <label for="exampleFormControlInput1">Author</label>
                 <input type="text" name="author" class="form-control" placeholder="John Doe">
+                <?php if (isset($errors['author'])) : ?>
+                    <div class="text-danger"><?php echo $errors['author']; ?></div>
+                <?php endif; ?>
             </div>
             <div class="form-group">
                 <label for="exampleFormControlInput1">Year</label>
                 <input type="number" name="year" class="form-control" placeholder="1984">
+                <?php if (isset($errors['year'])) : ?>
+                    <div class="text-danger"><?php echo $errors['year']; ?></div>
+                <?php endif; ?>
             </div>
             <div class="form-group">
                 <label for="exampleFormControlSelect1">Genre</label>
@@ -69,8 +97,11 @@ if (isset($_POST['submit'])) {
                 </select>
             </div>
             <div class="form-group">
-                <label for="exampleFormControlTextarea1">Decription</label>
+                <label for="exampleFormControlTextarea1">Description</label>
                 <textarea class="form-control" name="description" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <?php if (isset($errors['description'])) : ?>
+                    <div class="text-danger"><?php echo $errors['description']; ?></div>
+                <?php endif; ?>
             </div>
             <button type="submit" class="btn btn-success" name="submit">Add book</button>
         </form>
